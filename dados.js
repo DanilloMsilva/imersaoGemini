e7998a191f97b1c14c2b711815b511c30d70f863
DanilloMsilva <danillosilvamacario2002@gmail.com>
2024-09-09 23:16:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Neymar Jr",
+      descricao: "Neymar Jr: Neymar da Silva Santos Júnior, mais conhecido como Neymar Jr., é um dos maiores craques do futebol mundial. Desde seus primeiros passos no Santos, encantou a todos com sua habilidade inigualável e dribles desconcertantes. Sua transferência para o Barcelona o colocou no centro das atenções, onde brilhou ao lado de Messi e Suárez, formando um dos trios de ataque mais temidos da história.",
+      link: "https://pt.wikipedia.org/wiki/Neymar",
+      tags: "futebol brasil atacante neymarjr santos barcelona psg"
+    },
+    {
+      titulo: "Lionel Messi",
+      descricao: "Lionel Messi: Considerado um dos maiores jogadores de todos os tempos, Messi conquistou inúmeros títulos com o Barcelona e a seleção argentina. Sua habilidade técnica e visão de jogo são incomparáveis.",
+      link: "https://pt.wikipedia.org/wiki.org/wiki/Lionel_Messi",
+      tags: "futebol argentina atacante messi barcelona psg"
+    },
+    {
+      titulo: "Cristiano Ronaldo",
+      descricao: "Cristiano Ronaldo: Um dos maiores artilheiros da história do futebol, Ronaldo é conhecido por sua força física, velocidade e habilidade de marcar gols. Atualmente, ele joga pelo Al-Nassr.",
+      link: "https://pt.wikipedia.org/wiki.org/wiki.org/Cristiano_Ronaldo",
+      tags: "futebol portugal atacante cristianoronaldo manchesterunited realmadrid juventus alnassr"
+    }
+  ];
